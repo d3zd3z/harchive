6ee3831911cc7ef381ca70969672233a90f7ed9f
David Brown <harchive@davidb.org>
2008-12-02 00:26:57 -0800

    Pool: Scan existing pool files (without recovery).
    Scan the existing pool files, and fail if their sizes or count do not
    match the database.  This makes it clear where the recovery code needs
    to be placed.

--- src/Pool.hs
+++ src/Pool.hs
@@ -12,11 +12,17 @@ module Pool (
 
 import Hash
 import HexDump
+import Chunk.IO
 
 import qualified Database.HDBC as SQL
 import qualified Database.HDBC.Sqlite3 as SQL
 
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+
+import Numeric
 import System.Directory
+import System.FilePath
 
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString as B
@@ -36,19 +42,23 @@ data PoolState = PoolState {
    -- Since we aren't dynamically choosing different databases,
    -- there's no need to deal with polymorphism here.
    -- connection :: forall a. SQL.IConnection a => a
-   connection :: SQL.Connection }
+   connection :: SQL.Connection,
+   -- The currently accessible chunkfiles, indexed by cfile number.
+   chunkFiles :: IntMap ChunkFile }
 
 -- |Perform the given actions on the pool at the specified path.
 runPool :: FilePath -> StoragePool a -> IO a
 runPool path actions = do
    -- TODO: Verify the pool path.
    validatePath path
-   let dbName = path ++ "/" ++ databaseName
+   let dbName = path </> databaseName
    bracket (SQL.connectSqlite3 dbName) SQL.disconnect $ \db -> do
       setupSchema db
+      cf <- scanDataFiles path db
       let state0 = PoolState {
 	 basePath = path,
-	 connection = db }
+	 connection = db,
+	 chunkFiles = cf }
       mvar <- newMVar state0
       result <- runReaderT fullActions mvar
       return result
@@ -75,13 +85,95 @@ validatePath path = do
       c | databaseName `elem` c -> return ()
       _ -> fail "Pool path specified is not empty, and doesn't appear to be a pool"
 
+----------------------------------------------------------------------
 noDotty :: [FilePath] -> [FilePath]
 -- Eliminate Posix "." and ".." filenames from a directory list.
 -- TODO: This will move elsewhere.
 noDotty = filter $ \item -> item /= "." && item /= ".."
 
+----------------------------------------------------------------------
+scanDataFiles :: FilePath -> SQL.Connection -> IO (IntMap ChunkFile)
+-- Scan the data directory for data files, verifying that they are in
+-- the database, and perform recovery of the chunk information into
+-- the database.  Returns the chunkfile map.
+-- TODO: Recovery rather than just detection.
+scanDataFiles path db = do
+   cfileSizes <- queryCfiles db
+   let expectedSizes = map Just cfileSizes ++ repeat Nothing
+   let names = map (dataFileName path) [0..]
+   cfiles <- whileM scanFile (zip expectedSizes names)
+   return $ IntMap.fromList $ zip [0..] cfiles
+
+scanFile :: (Maybe Int, FilePath) -> IO (Maybe ChunkFile)
+-- Check a single pool data file, comparing it's size to the expected
+-- size.  Returns False when we should stop scanning.
+scanFile (expectedSize, path) = do
+   present <- doesFileExist path
+   case (present, expectedSize) of
+      (False, Just _) -> fail $ "Missing pool file: " ++ path
+      (False, Nothing) -> return Nothing
+      (True, Nothing) -> fail $ "Handle pool file not in DB: " ++ path
+      (True, Just size) -> do
+	 cfile <- openChunkFile path
+	 size' <- chunkFileSize cfile
+	 chunkClose cfile
+	 if size == size'
+	    then return $ Just cfile
+	    else fail $ "Pool size is wrong, need to recover: " ++ path
+
+{-
+whileM_ :: (a -> IO Bool) -> [a] -> IO ()
+-- For each element in the list, invoke the action on it, stopping
+-- when the action results in False.
+whileM_ _ [] = return ()
+whileM_ action (x:xs) = do
+   continue <- action x
+   if continue
+      then whileM_ action xs
+      else return ()
+-}
+
+whileM :: (a -> IO (Maybe b)) -> [a] -> IO [b]
+-- Invoke the action on each element of the list, accumulating the
+-- results as long as the action returns Just something.
+whileM _ [] = return []
+whileM action (x:xs) = do
+   y <- action x
+   case y of
+      Nothing -> return []
+      Just y' -> do
+	 ys <- whileM action xs
+	 return $ y' : ys
+
 databaseName :: String
 databaseName = "pool-info.sqlite3"
+
+dataFileName :: FilePath -> Int -> FilePath
+-- Construct the name of an individual datafile.
+dataFileName base index =
+   base </> "pool-data-" ++ padded ++ ".data"
+   where
+      padded = replicate (4 - length decimal) '0' ++ decimal
+      decimal = showInt index ""
+
+----------------------------------------------------------------------
+queryCfiles :: SQL.Connection -> IO [Int]
+-- Retrieve the sizes of each chunk file from the database.  Verifies
+-- that the files are present and in order.
+queryCfiles db = do
+   rows <- SQL.quickQuery' db
+      "select num, size from chunk_files order by num" []
+   return $ flattenSizes 0 rows
+   where
+      flattenSizes :: Int -> [[SQL.SqlValue]] -> [Int]
+      -- ensure that the query result from the database is sane.
+      flattenSizes _ [] = []
+      flattenSizes n ([n2, size]:xs) =
+	 if n == n2'
+	    then SQL.fromSql size : flattenSizes (n+1) xs
+	    else error "Unexpected sequence in chunk_files table"
+	 where n2' = SQL.fromSql n2
+      flattenSizes _ _ = error "Unexpected result from chunk_files query"
 
 ----------------------------------------------------------------------
 setupSchema :: SQL.Connection -> IO ()
